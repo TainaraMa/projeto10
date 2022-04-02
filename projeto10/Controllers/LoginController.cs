288937404d7078ae001b875084fa37e4a21adee9
TAINARA SILVA <tainara.silva6@fatec.sp.gov.br>
2022-04-01 22:03:49 -0300

    Adicionar arquivos de projeto.

---
+++ projeto10/Controllers/LoginController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace projeto10.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Entrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Entrar(string usuario, string senha)
+        {
+            if(
+                (usuario == "admin" && senha == "123")
+                ||
+              (usuario == "aluno" && senha == "aluno")
+              )
+            {
+                return RedirectToAction("Logado");
+            }
+            else
+            {
+                return RedirectToAction("NaoLogado");
+            }
+        }
+
+        public IActionResult Logado()
+        {
+            return View();
+        }
+
+        public IActionResult NaoLogado()
+        {
+            return View();
+        }
+    }
+}
